41e454006a4cae1981a6bd4ee4ab4926b40df7a7
aryan <aryan@gmail.com>
2024-03-03 21:52:02 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import path from  "path";
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
@@ -13,6 +13,8 @@ import { app,server } from "./socket/soket.js";
 dotenv.config()
 const PORT=process.env.PORT || 5000;
 
+const __dirname= path.resolve()
+
 
 app.use(express.json())
 app.use(cookieParser())
@@ -21,10 +23,14 @@ app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
-app.get( "/", (req,res)=>{
-    res.send("Hello World")
+// Handling any other routes that are not found in the above handlers
+app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname , "frontend","dist","index.html"))
 })
+
+
 
 
 server.listen(PORT,()=>{
